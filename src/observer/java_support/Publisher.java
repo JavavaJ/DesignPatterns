40334abb5472df9e4b5208094b3eec30522ff744
alex <fedotenko.aleksey@gmail.com>
2020-04-15 15:08:10 +0300

    Java built-in PropertyChangeListener implementation of Observer pattern

---
+++ src/observer/java_support/Publisher.java
@@ -0,0 +1,21 @@
+package observer.java_support;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class Publisher {
+
+    private String myValue;
+
+    PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+
+    public void registerObserver(PropertyChangeListener observer) {
+        propertyChangeSupport.addPropertyChangeListener(observer);
+    }
+
+    public void setValue(String newValue) {
+        String oldValue = this.myValue;
+        this.myValue = newValue;
+        propertyChangeSupport.firePropertyChange("myValue", oldValue, newValue);
+    }
+}
