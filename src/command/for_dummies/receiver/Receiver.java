5572cb09e74737108f4438b0bdc0502b5aab11e5
alex <fedotenko.aleksey@gmail.com>
2020-04-15 22:21:32 +0300

    Eliminated some whitespaces

--- src/command/for_dummies/receiver/Receiver.java
+++ src/command/for_dummies/receiver/Receiver.java
@@ -1,11 +1,9 @@
 package command.for_dummies.receiver;
 
 public interface Receiver {
-
     void connect();
     void diagnostics();
     void reboot();
     void shutdown();
     void disconnect();
-
 }
